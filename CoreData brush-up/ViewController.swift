45137c3e199d0d3f4a1f718ff2ee282423dca3d6
Tiago Do Couto <cs.couto@outlook.com>
2018-09-28 11:39:24 -0500

    Initial Commit

---
+++ CoreData brush-up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData brush-up
+//
+//  Created by Tiago Do Couto on 9/28/18.
+//  Copyright © 2018 Couto Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
